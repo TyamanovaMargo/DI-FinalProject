b115a8706197204f212c82f382d1e38c264323f5
Margo <109484645+TyamanovaMargo@users.noreply.github.com>
2025-04-27 18:45:35 +0300

    ch

--- yad2scrapper-master/scrapper/run.py
+++ yad2scrapper-master/scrapper/run.py
@@ -8,88 +8,83 @@ from modules.sendEmail import sendEmail
 import os
 import time
 import re
-import time
 import logging
-import os
-import re
-from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from argparse import ArgumentParser
 from selenium.webdriver.common.by import By
 
-# Настройка логирования
+# Setup logging configuration to log errors, info, etc.
 LOG_FILENAME = 'webScrapper.log'
 format = '%(asctime)s %(name)-15s %(levelname)-8s %(processName)-10s[%(process)d]%(funcName)10s: %(message)s'
 logging.basicConfig(filename=LOG_FILENAME, format=format, level=logging.DEBUG)
 logger = logging.getLogger('webScrapper')
 
+# Regex pattern to match URLs
 urlMatcher = re.compile(r'https?://[^/]+')
+
+# Get the current timestamp in a readable format
 ts = time.time()
 timeStamp = datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H:%M:%S')
 
+# Function to validate URL format
 def validateUrl(url):
     if urlMatcher.search(url):
         return True
     return False
 
+# Initialize temporary directories for storing data if they do not exist
 def initDirs():
     if not os.path.exists('./tmp'):
         os.mkdir('./tmp')
 
+# Function to scrape data from a URL, with an option for proxy and delay between requests
 def scrapUrlData(outFile, useProxy, delayInterval):
     options = Options()
     
-    # Устанавливаем прокси, если указан флаг useProxy
+    # Set proxy settings if the useProxy flag is enabled
     if useProxy:
         options.set_preference("network.proxy.type", 1)
         options.set_preference("network.proxy.http", "45.6.18.232")
         options.set_preference("network.proxy.http_port", 8080)
-        # добавьте настройки для других типов прокси, если нужно
 
-    # Инициализация драйвера Firefox с параметрами
+    # Initialize Firefox driver with specified options
     driver = webdriver.Firefox(options=options)
     driver.get(outFile)
 
-    # Задержка между запросами
+    # Delay between requests if specified
     if delayInterval:
         time.sleep(delayInterval)
 
-    # Ваш код для сбора данных с сайта
-    data = collectData(driver)  # предполагаем, что есть функция для сбора данных
+    # Collect data from the page
+    data = collectData(driver)
     driver.quit()
 
     return data
 
+# Function to collect data from the webpage by searching for listing items
 def collectData(driver):
-    # Функция для сбора данных с веб-страницы
     data = []
-
+    
     try:
-        # Находим все элементы, которые представляют собой объявления
+        # Find all listing items on the page by their class name
         items = driver.find_elements(By.CLASS_NAME, 'listing-item')
 
-        # Перебираем все найденные элементы
+        # Loop through each item and extract name and price
         for item in items:
             try:
-                # Извлекаем название и цену из каждого элемента
                 name = item.find_element(By.CLASS_NAME, 'title').text
                 price = item.find_element(By.CLASS_NAME, 'price').text
-
-                # Добавляем данные в список
                 data.append({"name": name, "price": price})
-
             except Exception as e:
-                # В случае ошибки (если название или цена не найдены) пропускаем элемент
                 print(f"Error extracting data from item: {e}")
                 continue
 
     except Exception as e:
-        # В случае ошибки при поиске элементов на странице
         print(f"Error extracting items from the page: {e}")
     
     return data
 
+# Function to dump collected data into a CSV file
 def dumpDataFile(data, pageNum, outFile):
     if data:
         with open(outFile, 'a') as f:
@@ -98,28 +93,33 @@ def dumpDataFile(data, pageNum, outFile):
     else:
         print(f"[ERROR]. No data collected for page {pageNum}")
 
+# Function to prepare the final report after scraping
 def prepareReport(outFile, sPageNum, ePageNum):
     print(f"[>]. Preparing report from {sPageNum} to {ePageNum}")
-    # Логика подготовки отчета (например, объединение всех данных в один файл)
 
+# Function to send an email with the output file and report
 def sendEmail(outFile, sPageNum, ePageNum, startTime, endTime):
     print(f"[>]. Sending email with file {outFile} and report")
 
-# Основной код
+# Main logic for argument parsing
 parser = ArgumentParser(description='Scrap data from YAD')
 parser.add_argument('filename', help='output filename')
 parser.add_argument('-u', '--url', dest='inputUrl', help='Provide a custom input URL for scraping')
-parser.add_argument('-s', '--start-page', type=int, dest='sPageNum', default=1, help='Provide the start page number from where you want to start scrapping')
-parser.add_argument('-e', '--end-page', type=int, dest='ePageNum', help='Provide the last page number upto which you want to do scrapping')
-parser.add_argument('-p', '--use-proxy', dest='useProxy', action='store_true', help='Enable proxy support for scrapping. Proxies are picked up from proxies configuration file "./db/proxies.txt"')
-parser.add_argument('-t', '--use-delay', dest='delayInterval', type=int, help='Provide a sleep delay for which program will sleep before launching every HTTP request')
+parser.add_argument('-s', '--start-page', type=int, dest='sPageNum', default=1, help='Provide the start page number')
+parser.add_argument('-e', '--end-page', type=int, dest='ePageNum', help='Provide the last page number')
+parser.add_argument('-p', '--use-proxy', dest='useProxy', action='store_true', help='Enable proxy support')
+parser.add_argument('-t', '--use-delay', dest='delayInterval', type=int, help='Provide a delay for each HTTP request')
 parser.add_argument('-C', '--cleanup', dest='cleanup', action='store_true', help='Perform a cleanup operation to remove temporary files')
 
+# Exclusive arguments for residential or commercial scraping
 group = parser.add_mutually_exclusive_group()
-group.add_argument('-r', '--residential', dest='isResidential', action='store_true', help="Start scrapping using default residential URL")
-group.add_argument('-c', '--commercial', dest='isCommercial', action='store_true', help="Start scrapping using default commercial URL")
+group.add_argument('-r', '--residential', dest='isResidential', action='store_true', help="Scrap residential listings")
+group.add_argument('-c', '--commercial', dest='isCommercial', action='store_true', help="Scrap commercial listings")
 
+# Parse the arguments
 args = parser.parse_args()
+
+# Extract parsed values
 filename = args.filename
 sPageNum = args.sPageNum
 ePageNum = args.ePageNum
@@ -127,91 +127,69 @@ useProxy = args.useProxy
 delayInterval = args.delayInterval
 inputUrl = args.inputUrl
 doCleanup = args.cleanup
-
 isResidential = args.isResidential
 isCommercial = args.isCommercial
 
+# Validate input and ensure that required arguments are provided
 if (not inputUrl) and (not (isCommercial or isResidential)):
     print("[!]. Please provide scrapping type either -r or -c\n")
     exit(1)
 
+# Validate URL if provided
 if inputUrl:
     inputUrl = inputUrl.strip()
     if not validateUrl(inputUrl):
-        print ("[ERROR]. URL pattern doesn't seem right")
+        print("[ERROR]. URL pattern doesn't seem right")
         exit(1)
 
-print ("\n")
-
+# Handle start and end page number logic
 if not inputUrl:
     if sPageNum and (not ePageNum):
-        print("[!]. End page number is not mentioned so only starting page number will be scrapped\n")
+        print("[!]. End page number is not mentioned")
         ePageNum = sPageNum
-
     if sPageNum > ePageNum:
-        print("[ERROR]. Start page number should be lesser than ending page number\n")
+        print("[ERROR]. Start page number should be less than end page number")
         exit(1)
 
-    if ePageNum and (not sPageNum):
-        print ("[!]. Please provide start page number as well\n")
-        exit(1)
-elif inputUrl:
-    print("[!]. Start and End page ranges are ignored in case of custom URL\n")
-if useProxy:
-    print("[>]. Firefox proxy profile enabled\n")
+# Initialize the URL based on the scraping type
+rawTargetUrl = "https://www.yad2.co.il/realestate/forsale?Page={}"
+if isCommercial:
+    rawTargetUrl = "http://www.yad2.co.il/Nadlan/business.php?Page={}"
+elif isResidential:
+    rawTargetUrl = "http://www.yad2.co.il/Nadlan/sales.php?Page={}"
 
-if delayInterval:
-    print("[>]. Configured a {} seconds delay for each HTTP request\n".format(delayInterval))
-
-outFile = "{}-{}.csv".format(filename, timeStamp)
-
-print("[!]. Task launched at '{}'".format(timeStamp))
-
-targetUrl = None
-isFirstPage = True
-exceptionCount = 0
-
-#initialize output directories
+# Prepare for scraping by initializing directories
 initDirs()
 
-# rawTargetUrl = "http://www.yad2.co.il/Nadlan/sales.php?multiSearch=1&AreaID=&City=&HomeTypeID=&fromRooms=&untilRooms=&fromPrice=50000&untilPrice=&PriceType=1&fromSquareMeter=&untilSquareMeter=&FromFloor=&ToFloor=&Info=&PriceOnly=1&Order=price&Page={}"
-rawTargetUrl = "https://www.yad2.co.il/realestate/forsale?Page={}"
-
-
-if isCommercial:
-    rawTargetUrl = "http://www.yad2.co.il/Nadlan/business.php?AreaID=&City=&Sale=&HomeTypeID=&fromSquareMeter=&untilSquareMeter=&fromPrice=1000&untilPrice=&PriceType=1&fromRooms=&untilRooms=&Info=&PriceOnly=1&Page={}"
-    
-elif isResidential:
-    rawTargetUrl = "http://www.yad2.co.il/Nadlan/sales.php?multiSearch=1&AreaID=&City=&HomeTypeID=&fromRooms=&untilRooms=&fromPrice=50000&untilPrice=&PriceType=1&fromSquareMeter=&untilSquareMeter=&FromFloor=&ToFloor=&Info=&PriceOnly=1&Order=price&Page={}"
-
+# Loop through pages and collect data
+exceptionCount = 0
 exceptionIDs = []
 if not inputUrl:
-    for pageNum in range(sPageNum, ePageNum +1):
+    for pageNum in range(sPageNum, ePageNum + 1):
         targetUrl = rawTargetUrl.format(pageNum)
-        print("[>]. Scrapping Page {}: {}\n".format(pageNum, targetUrl))
-
+        print(f"[>]. Scraping Page {pageNum}: {targetUrl}")
         try:
             outputData = scrapUrlData(targetUrl, useProxy, delayInterval)
         except Exception as e:
             exceptionCount += 1
             exceptionIDs.append(pageNum)
             print(e)
-            print("[!]. Exception happened at page {}".format(pageNum))
+            print(f"[!]. Exception happened at page {pageNum}")
         
         print("[!]. Dumping data into output file")
         dumpDataFile(outputData, pageNum, outFile=outFile)
 
-    #prepare report by joining all part files
+    # Prepare a final report
     prepareReport(outFile, sPageNum, ePageNum)
 
-    #cleanup data
+    # Perform cleanup if needed
     if doCleanup:
-        print("[!]. Performing a cleanup operations at path: {}".format('./tmp'))
+        print("[!]. Performing a cleanup operation")
 
 elif inputUrl:
+    # Scraping a single custom URL
     targetUrl = inputUrl
-    print("[>]. Scrapping Url: {}\n".format(targetUrl))
-
+    print(f"[>]. Scraping URL: {targetUrl}")
     try:
         outputData = scrapUrlData(targetUrl, useProxy, delayInterval)
     except Exception as e:
@@ -220,15 +198,12 @@ elif inputUrl:
     print("[!]. Dumping data into output file")
     dumpDataFile(outputData, -1, outFile=outFile)
 
+# Send an email after completion
 endTime = time.time()
 endTimeStamp = datetime.fromtimestamp(endTime).strftime('%Y-%m-%d-%H:%M:%S')
-
 if os.path.exists(outFile):
-    if inputUrl:
-        sendEmail(outFile,1 ,1, timeStamp, endTimeStamp)
-    else:
-        sendEmail(outFile, sPageNum, ePageNum, timeStamp, endTimeStamp)
+    sendEmail(outFile, sPageNum, ePageNum, timeStamp, endTimeStamp)
 
-print("[!]. Task completed at '{}'".format(endTimeStamp))
-print("[!]. Total encountered Exceptions: {}".format(exceptionCount))
-print("[!]. Encountered Exception IDs: {}".format(', '.join(exceptionIDs)))
+print(f"[!]. Task completed at '{endTimeStamp}'")
+print(f"[!]. Total encountered Exceptions: {exceptionCount}")
+print(f"[!]. Encountered Exception IDs: {', '.join(exceptionIDs)}")
