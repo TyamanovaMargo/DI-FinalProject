4c58edf88635713a298816e82497150a18bbdb77
Margo <109484645+TyamanovaMargo@users.noreply.github.com>
2025-04-28 10:33:59 +0300

    work vers based

--- yad2scrapper-master/scrapper/test.py
+++ yad2scrapper-master/scrapper/test.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import time
 import pandas as pd
+import re
 
 # Setup Chrome options
 chrome_options = Options()
@@ -31,6 +32,24 @@ for _ in range(3):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(2)  # wait for more items to load
 
+
+def extract_count(parts, parameter):
+   
+    patterns = {
+        1: r'(\d+)\s+חדרים',
+        2: r'קומה\s+(\d+)',
+        3: r'(\d+)\s+מ״ר'
+    }
+    
+    if parameter in patterns:
+        match = re.search(patterns[parameter], parts)
+        if match:
+            return match.group(1)
+        else:
+            return None
+    else:
+        return None
+    
 
 # Get listings
 listings = driver.find_elements(By.CSS_SELECTOR, "li[data-testid='item-basic']")
@@ -58,15 +77,25 @@ for listing in listings:
         link = None
     try:
         content = listing.find_element(By.XPATH, './/h2[@data-nagish="content-section-title"]').text
+        parts = content.split('•')
+        # print(parts)
+        area_parts = parts[-1].strip() 
+        area_size = extract_count(area_parts,3)
+        # print(area)   
+        floor_parts = parts[-2].strip()
+        floor_count = extract_count(floor_parts,2)
 
+        rooms_parts = parts[-3].strip()
+        rooms_count = extract_count(rooms_parts,1)
+        
     except:
         link = None
     try:
         property_type = info = listing.find_element(By.XPATH, './/span[contains(@class, "item-data-content_itemInfoLine__AeoPP")]').text
         parts = property_type.split(',')
         property_type = parts[0].strip() if len(parts) > 0 else None
-        city = parts[1].strip() if len(parts) > 1 else None
-        neighborhood = parts[2].strip() if len(parts) > 2 else None
+        neighborhood = parts[1].strip() if len(parts) > 1 else None
+        city = parts[2].strip() if len(parts) > 2 else None
     except:
         link = None
 
@@ -79,26 +108,27 @@ for listing in listings:
     #     "City": city,
     #     "Neighborhood": neighborhood
     # })
-    # Добавляем всё в список
+    # add to the list
     data.append({
         "Title": address,
         "Price": price,
         "Link": link,
-        "Content": content,
         "Property Type": property_type,
+        "Rooms_count": rooms_count,
+        "Floor": floor_count,
+        "Area": area_size,
         "City": city,
         "Neighborhood": neighborhood
     })
 
-# Закрываем браузер
-driver.quit()
+
 
 # Превращаем список в pandas DataFrame
 df = pd.DataFrame(data)
 
 # Сохраняем в CSV
-df.to_csv('yad2_listings.csv', index=False, encoding='utf-8-sig')
+df.to_csv('yad2_listings2.csv', index=False, encoding='utf-8-sig')
 
-print("✅ CSV файл сохранен как yad2_listings.csv")
+print("✅ CSV файл сохранен как yad2_listings2.csv")
 # Close browser
-# driver.quit()
+driver.quit()
